13e08b229e02db5f882a265f0141be7afe8e47d0
KainSt <ksfgnw@gmail.com>
2022-02-07 00:26:29 +0300

    рабочая версия

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,10 +1,74 @@
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 
-    private static Document
+    private static Document getPage() throws IOException {
+        String url = "https://pogoda.spb.ru";
+        return Jsoup.parse(new URL(url),3000);
+    }
 
-    public static void main(String[] args){
+    private static int printLineValues(Elements values, int index){
+        int iterationCount = 4;
+        if (index==0){
+            Element valueLn = values.get(3);
+            boolean isMorning = valueLn.text().contains("Утро");
+            valueLn = values.get(4);
+            boolean isNight = valueLn.text().contains("Ночь");
+
+
+        if (isMorning) {
+            iterationCount = 3;
+        }
+        if (isNight) {
+            iterationCount = 5;
+        }
+            }
+         for (int i = 0; i < iterationCount; i++) {
+         Element valueLine = values.get(index + i);
+         for (Element td : valueLine.select("td")) {
+             System.out.print(td.text() + "    ");
+         }
+          System.out.println();
+     }
+
+
+        return iterationCount;
+    }
+    //06.02 Воскресенье погода сегодня
+    //06.02
+    // \d{2}\.\d{2}
+    private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
+
+    private static String getDateFromString (String stringDate) throws Exception {
+        Matcher matcher = pattern.matcher(stringDate);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        throw new Exception("Can't extract date from string");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Document page = getPage();
+        // css query language
+        Element tableWth = page.select("table[class=wt]").first();
+        Elements names = tableWth.select("tr[class=wth]");
+        Elements values = tableWth.select("tr[valign=top]");
+        int index=0;
+        for (Element name: names) {
+            String dateString = name.select("th[id=dt]").text();
+            String date = getDateFromString(dateString);
+            System.out.println(date + " Явление Температура Давление Влажность Ветер");
+            int iterationCount = printLineValues(values,index);
+            index = index + iterationCount;
+        }
 
     }
 }
